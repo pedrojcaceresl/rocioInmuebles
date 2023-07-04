b3253280e1d5f30a275f07445c8af95e37d45176
pedro <pedro.jcaceresl@gmail.com>
2023-07-03 21:54:14 -0400

    refactor(propiedades-page): el componente propiedades ya se carga desde un json file

--- src/app/propiedades/pages/propiedades-page/propiedades-page.component.ts
+++ src/app/propiedades/pages/propiedades-page/propiedades-page.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   templateUrl: './propiedades-page.component.html',
   styleUrls: ['./propiedades-page.component.scss']
 })
 export class PropiedadesPageComponent {
+  public propiedades : any[] = [];
 
+  http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http.get('/assets/propiedades.json').subscribe((res: any) => {
+      console.log(res);
+      this.propiedades = res;
+    });
+  }
 }
