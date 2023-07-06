46b79fee454dabd734d0100e3cbf1856598c9478
pedro <pedro.jcaceresl@gmail.com>
2023-07-05 20:12:31 -0400

    feat(nueva-propiedad.maps): Añadido la posibilidad de guardar la coordenada en el formulario

--- src/app/modules/admin/pages/nueva-propiedad-page/nueva-propiedad-page.component.ts
+++ src/app/modules/admin/pages/nueva-propiedad-page/nueva-propiedad-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {
   FormBuilder,
+  FormControl,
+  FormGroup,
   Validators,
 } from '@angular/forms';
 
@@ -9,23 +11,34 @@ import {
   styleUrls: ['./nueva-propiedad-page.component.scss'],
 })
 export class NuevaPropiedadPageComponent {
+  locationForm = new FormGroup({
+    latitude: new FormControl(),
+    longitude: new FormControl(),
+  });
+
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
+
   secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+    latitude: [0],
+    longitude: [0]
   });
+
   isLinear = false;
 
   mapInitialized?: google.maps.Map;
-  selectedPosition!: google.maps.LatLng;
-
+  selectedPosition!: google.maps.LatLng | null | undefined;
+  marker!: google.maps.Marker;
+  lat: any = -25.496983038652843;
+  lng: any = -54.6637051698589;
 
   zoom = 12;
   center: google.maps.LatLngLiteral = {
     lat: -25.496983038652843,
     lng: -54.6637051698589,
   };
+
   options: google.maps.MapOptions = {
     mapTypeId: 'hybrid',
     zoomControl: false,
@@ -37,20 +50,38 @@ export class NuevaPropiedadPageComponent {
 
   onMapInitialized(map: google.maps.Map) {
     this.mapInitialized = map;
+    this.initMarker();
   }
 
   get map() {
     return this.mapInitialized;
   }
 
-  addMarker(event: any) {
-    this.selectedPosition = event.latLng
+  initMarker() {
+    this.marker = new google.maps.Marker({
+      position: { lat: this.lat, lng: this.lng },
+      map: this.map,
+      draggable: true,
+      // label: 'Hello'
+    });
 
-      new google.maps.Marker({
-        position: event.latLng,
-        map: this.map,
-        title: 'Hello World!',
-        draggable: true
-      });
+
+    this.marker.setAnimation(google.maps.Animation.DROP);
+    this.marker.addListener('dragend', () => {
+      this.selectedPosition = this.marker.getPosition();
+      const position = this.marker.getPosition();
+      this.lat = position?.lat();
+      this.lng = position?.lng();
+      this.updateFormValues();
+    });
+  }
+
+  updateFormValues() {
+    this.secondFormGroup.patchValue({
+      latitude: this.lat,
+      longitude: this.lng,
+    });
+
+    console.log(this.secondFormGroup.value);
   }
 }
