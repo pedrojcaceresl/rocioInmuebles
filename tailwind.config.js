038a5b3f78a712d802a8d364ba2866d80daebd0b
pedro <pedro.jcaceresl@gmail.com>
2023-07-09 08:08:04 -0400

    (style):implementado dark y light al linktree

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  darkMode: "media",
+
   safelist: [
     "w-64",
     "w-1/2",
