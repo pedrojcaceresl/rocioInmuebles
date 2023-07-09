40c64d90dae4bebfc3f30cf4a681a635d0f106f3
pedro <pedro.jcaceresl@gmail.com>
2023-07-09 09:28:59 -0400

    feat(propiedades): añadido el componente de propiedades skeleton

---
+++ src/app/modules/client/propiedades/components/propiedades-card-skeleton/propiedades-card-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propiedades-card-skeleton',
+  templateUrl: './propiedades-card-skeleton.component.html',
+  styleUrls: ['./propiedades-card-skeleton.component.scss']
+})
+export class PropiedadesCardSkeletonComponent {
+
+}
--- src/app/modules/client/propiedades/propiedades.module.ts
+++ src/app/modules/client/propiedades/propiedades.module.ts
@@ -5,12 +5,14 @@ import { PropiedadesRoutingModule } from './propiedades-routing.module';
 import { PropiedadesPageComponent } from './pages/propiedades-page/propiedades-page.component';
 import { PropiedadesCardComponent } from './components/propiedades-card/propiedades-card.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PropiedadesCardSkeletonComponent } from './components/propiedades-card-skeleton/propiedades-card-skeleton.component';
 
 
 @NgModule({
   declarations: [
     PropiedadesPageComponent,
-    PropiedadesCardComponent
+    PropiedadesCardComponent,
+    PropiedadesCardSkeletonComponent
   ],
   imports: [
     CommonModule,
