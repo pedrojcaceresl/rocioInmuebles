9ae804949b37e1990a2c67c80b8817a46e2a4f0e
pedro <pedro.jcaceresl@gmail.com>
2024-05-12 22:51:41 -0400

    add: layout a propiedades

--- src/app/modules/client/propiedades/propiedades-routing.module.ts
+++ src/app/modules/client/propiedades/propiedades-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PropiedadesPageComponent } from './pages/propiedades-page/propiedades-page.component';
 import { PropiedadesDetallesPageComponent } from './pages/propiedades-detalles-page/propiedades-detalles-page.component';
+import { PropertiesPageComponent } from '../propertiesPage/propertiesPage.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PropiedadesPageComponent,
-  },
-  {
-    path: 'detalle/:id',
-    component: PropiedadesDetallesPageComponent,
+    children: [
+      {
+        path: '',
+        component: PropertiesPageComponent,
+      },
+      {
+        path: 'detalle/:id',
+        component: PropiedadesDetallesPageComponent,
+      },
+    ],
   },
 ];
 
