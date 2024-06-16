8defffde9b38524339f1181a475563f0898b4ecf
pedro <pedro.jcaceresl@gmail.com>
2024-06-16 17:10:49 -0400

    Active route fix navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { last } from 'rxjs';
+
+
+type MenuItem = {
+  route: string;
+  title: string;
+}
 
 @Component({
   selector: 'shared-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  menuItems: MenuItem[] = [
+    {
+      route: '/propiedades',
+      title: 'propiedades',
+    },
+    {
+      route: '/mapa',
+      title: 'mapa',
+    },
+    {
+      route: '/about',
+      title: 'sobre mi',
+    },
+  ];
 }
